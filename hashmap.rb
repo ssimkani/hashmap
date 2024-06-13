da4a55e9a273e8e14330643a7d1addb7e6cd6fd8
ssimkani <seenasimkani@gmail.com>
2024-06-13 08:10:42 -0400

    Add initialize method to HashMap

--- hashmap.rb
+++ hashmap.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class HashMap
+  attr_reader :map, :length, :load_factor, :threshold
+
+  def initialize(size)
+    @map = Array.new(size)
+    @length = @map.compact.length
+    @load_factor = 0.8
+    @threshold = @length * @load_factor
+  end
+
   def hash(key)
     hash_code = 0
     prime_number = 31
