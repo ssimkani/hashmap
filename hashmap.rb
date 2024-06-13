4ce4cca00a9db9a1de15e9832568dbf6ed07370c
ssimkani <seenasimkani@gmail.com>
2024-06-13 07:55:43 -0400

    Add method for creating hash code

--- hashmap.rb
+++ hashmap.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class HashMap
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code
+  end
 end
